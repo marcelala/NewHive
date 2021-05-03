7244bdb08f88c5434c9d6ff99dcc6f27cbe56fa8
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-05-03 12:03:59 +0200

    Adds Community Guidelines

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 // NPM Packages
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import fontawesome components
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 // Project files
 import { Header } from "../src/components/Header";
 import { Home } from "../src/pages/Home";
+import  Feed  from "../src/pages/Feed";
+
+
 import "./styles/style.css";
 
 function App() {
@@ -13,6 +22,9 @@ function App() {
        <Header />
         <Switch>
         <Route component={Home} path="/" exact />
+        </Switch>
+        <Switch>
+        <Route component={Feed} path="/feed"/>
         </Switch>
        </BrowserRouter>
     </div>
---
+++ frontend/src/components/Comment/CommentCard.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import EditComment from "./EditComment";
+import CommentApi from "../../api/CommentApi";
+
+export default function CommentCard({ comment, onDeleteClick, user }) {
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  function userCheck() {
+    if (comment.userCommentOwner === user.email) {
+      return true;
+    }
+    return false;
+  }
+
+  function dateCreatedOrUpdatedCheck() {
+    if (comment.created === null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function date() {
+    if (dateCreatedOrUpdatedCheck()) {
+      const createDate = comment.created.substring(0, 10);
+      return `Created: ${createDate}`;
+    } else {
+      const updateDate = comment.updated.substring(0, 10);
+      return `Updated: ${updateDate}`;
+    }
+  }
+
+  async function updateComment(updatedComment) {
+    try {
+      await CommentApi.updateComment(updatedComment, comment.id);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <div className="commentCard">
+      <div className="commentCard__content">
+        <p>{comment.body}</p>
+        <p className="commentCard--user">{comment.userCommentOwner}</p>
+      </div>
+      <div className="commentCard--date">{date()}</div>
+      {userCheck() && (
+        <div>
+          <div className="commentCard__editDelete">
+            <div className="commentCard__Delete">
+              <FontAwesomeIcon
+                className="delete"
+                icon={["fa", "trash-alt"]}
+                onClick={onDeleteClick}
+              />
+            </div>
+            <p className="commentCard__editDelete--heading"></p>
+            <button
+              className="btn"
+              type="button"
+              onClick={() =>
+                toggleEdit ? setToggleEdit(false) : setToggleEdit(true)
+              }
+            >
+              Edit
+            </button>
+          </div>
+          {toggleEdit && (
+            <EditComment
+              onSubmit={(commentData) => updateComment(commentData)}
+              comment={comment}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
