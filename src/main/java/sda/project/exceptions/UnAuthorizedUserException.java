36fe4dd1d374932f53b296e85addaaf69e6ec226
Nirali Thaker <nirali24392@gmail.com>
2021-05-05 09:49:24 +0200

    Adding unfinished work into branch

---
+++ src/main/java/sda/project/exceptions/UnAuthorizedUserException.java
@@ -0,0 +1,9 @@
+package sda.project.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnAuthorizedUserException extends RuntimeException {
+}
---
+++ src/main/java/sda/project/posts/PostController.java
@@ -0,0 +1,49 @@
+package sda.project.posts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+
+    }
+
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@RequestBody Post postParam){
+        return postService.create(postService.generatePost(postParam));
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getAllPosts(){
+        return postService.fetchAll();
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id){
+        return ResponseEntity.ok(postService.fetchPostById(id));
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> postUpdate(@PathVariable Long id, @RequestBody Post post){
+        Post existingPost = postService.fetchPostById(id);
+        return postService.create(postService.update( post, existingPost));
+    }
+
+    @DeleteMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable Long id){
+        postService.deletePostById(id);
+    }
+}
