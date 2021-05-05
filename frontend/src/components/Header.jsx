3a1147fff3d0c50146876174f0c005c6310074c3
Natalia <nataliafedorova1989@gmail.com>
2021-05-05 11:16:27 +0200

    add logout in nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,1 +1,22 @@
-export const Header = () => <div className="main-header">Header</div>
+import { Link } from "react-router-dom";
+
+export const Header = ({ onLogout }) => 
+    <div className="main-header">
+        <ul>
+            <li>
+                <Link to="/">
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link to="/feed">
+                    Feed
+                </Link>
+            </li>
+            <li>
+                <button onClick={onLogout}>
+                    Logout
+                </button>
+            </li>
+        </ul>
+    </div>
