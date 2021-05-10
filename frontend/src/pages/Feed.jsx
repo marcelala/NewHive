3558893d836869ca248c9a28849d0b494df2788a
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-05-10 11:51:32 +0200

    component for post sorting added, non-functional

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Select from 'react-select';
+
 
 // Project files
 import PostApi from "../api/PostApi";
 import PostCard from "../components/Post/PostCard";
 import PostForm from "../components/Post/PostForm";
 import Banner from "../components/Banner";
+import Topics from "../components/Topics";
 
 
 
@@ -13,7 +16,9 @@ export const Feed = () => {
     // Local state
   const [posts, setPosts] = useState([]);
   const [toggleForm, setToggleForm] = useState(false);
-
+  const sorterOptions=[
+    {value: 'dateAscending', label: 'Date ascending' },
+  { value: 'dateDescending', label: 'Date descending' }]
 
   // Methods
   async function createPost(postData) {
@@ -73,6 +78,24 @@ export const Feed = () => {
             <PostForm onSubmit={(postData) => createPost(postData)}/>
         </div>
       )}
+      <div className="Feed__topic-filter">
+      <Select className="topic-filter" 
+        placeholder= "Filter by topic"
+        labelKey="label"
+        valueKey="id"
+        options={Topics}
+        // onChange={(e) => setTopic(e.value)}
+        />
+        </div>
+        <div className="Feed__post-sorter">
+      <Select className="post-sorter" 
+        placeholder= "Sort by"
+        options={sorterOptions}
+        // onChange={(e) => setTopic(e.value)}
+        />
+        </div>
+
+
       {PostsArray}
     </div>
   );
