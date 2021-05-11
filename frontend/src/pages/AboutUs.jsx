c575d2599c1cf4971157d3ca89011085fd3354eb
Natalia <nataliafedorova1989@gmail.com>
2021-05-11 18:26:56 +0200

    add responsive styles to About page and Footer

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 export default function AboutUs() {
     //Properties
-    const aboutPictureObject = require (`../assets/images/about/about-picture.jpg`);
-    const about2PictureObject = require (`../assets/images/about/about2-picture.jpg`);
+    const aboutPictureObject = require (`../assets/images/about/about3-picture1.jpg`);
+    const about2PictureObject = require (`../assets/images/about/about2-picture1.jpg`);
     const aboutPictureURL = aboutPictureObject.default;
     const about2PictureURL = about2PictureObject.default;
 
     return (
-        <div className="about">
+        <div class="about">
             <h1>Community Name</h1>
             <h2>(Pitch) Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <div className="about-grid">
             <div className="about-section">
                 <div className="about-photo">
-                <img src={aboutPictureURL} alt="Three ladies are talking and looking happy"/>
+                    <img src={aboutPictureURL} alt="Three ladies are talking and looking happy"/>
                 </div>
                 <div className="about-content">
                     <h3>History</h3>
@@ -35,6 +37,8 @@ export default function AboutUs() {
                     <img src={about2PictureURL} alt="2 hands fist-bumping"/>
                 </div>
             </div>
+            </div>
+            <Footer/>
         </div>
     )
 }
