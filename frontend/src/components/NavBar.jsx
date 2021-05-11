5e1083b03e03c53ccce2f20e518eb0e83099e3f8
Natalia <nataliafedorova1989@gmail.com>
2021-05-11 11:53:20 +0200

    link in nav

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,10 +11,10 @@ export const NavBar = ({ onLogout }) => (
       <Link id="feed" className="menu-item" to="/feed">
         Feed
       </Link>
-      <Link id="mentorship" className="menu-item" href="/mentorship">
+      <Link id="mentorship" className="menu-item" to="/mentorship">
         Mentorship
       </Link>
-      <Link id="messages" className="menu-item" href="/messages">
+      <Link id="messages" className="menu-item" to="/messages">
         My messages
       </Link>
       <Link id="profile" className="menu-item" to="/profile">
