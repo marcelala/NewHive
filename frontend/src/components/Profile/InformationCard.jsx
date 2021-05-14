2b5065215741310355b2a08b65bf0a87ffd470bb
Natalia <nataliafedorova1989@gmail.com>
2021-05-14 14:09:17 +0200

    add styles to profile page and components - wip

--- frontend/src/components/Profile/InformationCard.jsx
+++ frontend/src/components/Profile/InformationCard.jsx
@@ -4,20 +4,26 @@ import ProfileApi from "../../api/ProfileApi";
 
 
 export default function InformationCard ({profileInfo}) {
-
+    const TestPhotoObject = require (`../../assets/images/profile/profile-photo.png`);
+    const TestPhotoURL = TestPhotoObject.default;
     return (
-        <div>
-            <div className="full-name">
-                <p>Name: {profileInfo.name}</p>
-                <p>Surname: {profileInfo.surname}</p>
-            </div>
-            <div className="profile-info">
-                <h2>Personal information</h2>
-                <p>I am from: {profileInfo.countryFrom}</p>
-                <p>I live in: {profileInfo.liveIn}</p>
-                <p>Bio: {profileInfo.bio}</p>
-                {/*<p>Open for mentoring others: {profileInfo.mentor}</p>*/}
-                <p>Can mentor in: {profileInfo.mentorArea}</p>
+        <div className="profile">
+            <div className="profile-grid">
+                <div className="profile-section">
+                    <div className="profile-photo">
+                        <img src={TestPhotoURL} alt="a portrait photo of a woman with a cup of tea"/>
+                    </div>
+                    <div className="profile-info">
+                        <h2>Personal information</h2>
+                        <p>Name: {profileInfo.name}</p>
+                        <p>Surname: {profileInfo.surname}</p>
+                        <p>I am from: {profileInfo.countryFrom}</p>
+                        <p>I live in: {profileInfo.liveIn}</p>
+                        <p>Bio: {profileInfo.bio}</p>
+                        {/*<p>Open for mentoring others: {profileInfo.mentor}</p>*/}
+                        <p>Can mentor in: {profileInfo.mentorArea}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
