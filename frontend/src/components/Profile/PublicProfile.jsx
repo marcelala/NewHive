fa5ef261f3bf23c38d4cf029ff68ba4854d4ca47
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-05-16 16:45:43 +0200

    public  profile page

---
+++ frontend/src/components/Profile/PublicProfile.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import ProfileApi from "../../api/ProfileApi";
+import UserCard from "../UserCard"
+
+export default function PublicProfile () {
+
+    return(
+)}
