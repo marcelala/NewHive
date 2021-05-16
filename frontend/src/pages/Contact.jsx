6d09c65a7dc02d778148563320df861642c35f29
olhapinchuk <pinchukolya@gmail.com>
2021-05-16 18:11:37 +0200

    cleanup

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,53 +1,76 @@
-import Select from 'react-select';
-import React from "react"; 
-import Footer from "../components/Footer";
+import Select from "react-select";
 
 export const Contact = () => {
-
   const options = [
-    { value: 'Question', label: 'Question' },
-    { value: 'Suggestions', label: 'Suggestions' },
-    { value: 'Feedback', label: 'Feedback' }];
-    
-    function validateEmail(input) {
+    { value: "Question", label: "Question" },
+    { value: "Suggestions", label: "Suggestions" },
+    { value: "Feedback", label: "Feedback" },
+  ];
 
-      var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    
-      if (input.value.match(validRegex)) {
-        return true;
-      } else {
-        alert("Invalid email address!");
-        return false;
-      }
+  function validateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.value.match(validRegex)) {
+      return true;
+    } else {
+      alert("Invalid email address!");
+      return false;
     }
-    return (
-<section class="contact__container">
-                <h2 className="contact__heading">Contact</h2>
-                <h4 className="contact__sub-heading">Have questions? We have answers! </h4>
-                  <h4>Contact us with this form or on other social media.</h4>
-          <div className="form contact-form">
-            <div className="input-group-wrap">
-              <div className="input-group">
-              <input type="text" className="input" placeholder="Enter your name..." required/>
-              <span className="bar"></span>
-              </div>
-              <div className="input-group">
-                <input type="email" className="input" placeholder="Enter your email..." required/>
-                <span className="bar"></span>
-              </div>
-            </div>
-            <div className="contact__selector-container">
-              <Select name="topic-filter" options={options} />
-              </div>
-              <span className="bar"></span>
-            <div className="input-group">
-              <textarea name="subject" id="subject" cols="30" rows="8" placeholder="Enter your message here..." required></textarea>
-              <span className="bar"></span>
-            </div>
-            <div className="cta">
-            <a href="mailto:info@community.com" className="form-group btn-group" onclick="validateEmail(email)">Send Message</a>
-            </div>
+  }
+  return (
+    <section className="contact__container">
+      <h2 className="contact__heading">Contact</h2>
+      <h4 className="contact__sub-heading">
+        Have questions? We have answers!{" "}
+      </h4>
+      <h4>Contact us with this form or on other social media.</h4>
+      <div className="form contact-form">
+        <div className="input-group-wrap">
+          <div className="input-group">
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter your name..."
+              required
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-group">
+            <input
+              type="email"
+              className="input"
+              placeholder="Enter your email..."
+              required
+            />
+            <span className="bar"></span>
+          </div>
         </div>
+        <div className="contact__selector-container">
+          <Select name="topic-filter" options={options} />
+        </div>
+        <span className="bar"></span>
+        <div className="input-group">
+          <textarea
+            name="subject"
+            id="subject"
+            cols="30"
+            rows="8"
+            placeholder="Enter your message here..."
+            required
+          ></textarea>
+          <span className="bar"></span>
+        </div>
+        <div className="cta">
+          <a
+            href="mailto:info@community.com"
+            className="form-group btn-group"
+            onclick="validateEmail(email)"
+          >
+            Send Message
+          </a>
+        </div>
+      </div>
     </section>
-    )
-}
+  );
+};
