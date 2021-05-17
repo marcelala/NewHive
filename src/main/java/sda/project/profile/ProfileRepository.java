79f5a846679bc3bb2538ae253629df0a7685ec60
Himani Paronigar <himanijkothari@gmail.com>
2021-05-17 15:19:57 +0200

    adding missing import

--- src/main/java/sda/project/profile/ProfileRepository.java
+++ src/main/java/sda/project/profile/ProfileRepository.java
@@ -3,7 +3,7 @@ package sda.project.profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-
+import org.springframework.stereotype.Repository;
 import sda.project.user.User;
 
 
