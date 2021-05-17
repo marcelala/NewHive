3520ed34ceefd15c13249b10d10ffa01da776535
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-05-17 17:52:06 +0200

    add filter by mentor area on mentors page

--- frontend/src/components/Mentor/MentorsPage.jsx
+++ frontend/src/components/Mentor/MentorsPage.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react";
+import Select from "react-select";
+
+//Project files
 import ProfileApi from "../../api/ProfileApi";
 import MentorCard from "../Mentor/MentorCard";
+import mentorAreas from "../../data/mentorAreas";
 
 export default function MentorsPage () {
     const [mentors, setMentors] = useState([]);
+
+    //Topic selector state
+    const [selectedArea, setSelectedArea] = useState(undefined);
+    const handleChange = (chosenValue) => {
+        if (chosenValue) {
+        setSelectedArea(chosenValue.value);
+        } else {
+        setSelectedArea(undefined);
+        }
+    };
 
     useEffect(() => {
         ProfileApi.getAllMentors()
@@ -11,7 +25,16 @@ export default function MentorsPage () {
         .catch((err) => console.error(err));
     }, [setMentors]);
 
-    const MentorsArray = mentors.map((mentor) => (
+      // Components
+    const mentorsToShow = mentors.filter((mentor) => {
+    if (selectedArea) {
+      return mentor.mentorArea === selectedArea;
+    } else {
+      return true;
+    }
+  });
+
+    const MentorsArray = mentorsToShow.map((mentor) => (
         <MentorCard key={mentor.id} mentorInfo={mentor}/>
     ))
 
@@ -22,6 +45,17 @@ export default function MentorsPage () {
             <div className="mentors-page-title">
                 <h1 id="mentor-h1">NewHive Mentors</h1>
                 <h2 id="mentor-h2">Get mentorship help from others!</h2>
+            </div>
+            <div className="feed__selectors">
+                <Select
+                isClearable
+                className="topic-filter"
+                placeholder="Choose field"
+                labelKey="label"
+                valueKey="id"
+                options={mentorAreas}
+                onChange={handleChange}
+                />
             </div>
             <div className="mentors-card">
             {MentorsArray}
