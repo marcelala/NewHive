76a93cd141e03c0a26a924277031880c05b79aa9
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-05-18 10:45:32 +0200

    follower api

---
+++ frontend/src/api/FollowerApi.js
@@ -0,0 +1,11 @@
+import Api from "./Api";
+
+class FollowerApi {
+    addFollower(id) {
+        return Api.get("/follower/add/" + id);
+    }
+    removeFollower(id) {
+        return Api.get("/unfollow/remove/" + id);
+    }
+
+}
