f7ca3da3ebcaf02ea0d9d4eb2ad435b6833c2ecd
olhapinchuk <pinchukolya@gmail.com>
2021-05-18 17:39:58 +0200

    add warnings, code cleanup

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import UserApi from "../api/UserApi";
-import ProfileApi from "../api/ProfileApi";
+import { useState } from "react";
 import Lady from "../assets/images/profile/profile-photo.png";
 
 export default function UserCard({ profileInfo }) {
@@ -11,7 +9,7 @@ export default function UserCard({ profileInfo }) {
     <section className="userCard" onClick={() =>
       toggleInfo ? setToggleInfo(false) : setToggleInfo(true)
     }>
-      <img src={Lady} className="userCard__avatar" />
+      <img src={Lady} className="userCard__avatar" alt=""/>
         <p className="userCard__full-name">{name} {surname}</p>
         <div className="userCard__text">
       <p className="userCard__origin">
@@ -22,7 +20,7 @@ export default function UserCard({ profileInfo }) {
       <div className="userCard__info">
       {/* <p className="userCard__email">{profileInfo.email}</p> */}
       <p className="userCard__mentorship">
-        I can mentor others in {profileInfo.mentorArea}
+        I can mentor others in {mentorArea}
       </p>
       <br/>
       <p className="userCard__bio"> Bio: </p>
