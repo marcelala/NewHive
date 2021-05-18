fd3cb67271ddb38f7ac44c7ddcfe99ae97211adb
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-05-19 00:53:41 +0200

    fixes read more bug and adds logo to banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,13 @@
 function Banner(){
 
-
+    const photoObject = require ("../assets/images/NewHive-brown.png")
+    const photoURL = photoObject.default;
     return (
     <section className="feed-welcome">
         <div className="feed--background-banner">
+        <div className="logo-banner">
+        <img src={photoURL} className="logo-newHive" alt="honeycomb" />
+      </div>
         <div className="feed--banner-text">
             <h3>Connecting women to  </h3>
             <h2>their new empowering networks</h2>
